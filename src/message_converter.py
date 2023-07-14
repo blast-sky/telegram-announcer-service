3dc3f494c2ed3d561203c7b81cf6b439f428d91c
blast-sky <strogancev2020@gmail.com>
2023-07-15 01:58:56 +0300

    set explicit encoding

--- src/message_converter.py
+++ src/message_converter.py
@@ -1,44 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def convert_to_markdown(vacancy: dict) -> str:
-    markdown = "__Новая вакансия, удовлетворяющая вашему фильтру:__\n"
+    markdown = "__РќРѕРІР°СЏ РІР°РєР°РЅСЃРёСЏ, СѓРґРѕРІР»РµС‚РІРѕСЂСЏСЋС‰Р°СЏ РІР°С€РµРјСѓ С„РёР»СЊС‚СЂСѓ:__\n"
 
     if vacancy.get("name"):
-        markdown += f"\n**Название:** {vacancy['name']}"
+        markdown += f"\n**РќР°Р·РІР°РЅРёРµ:** {vacancy['name']}"
 
     if vacancy.get("url"):
         markdown += f"\n**URL:** {vacancy['url']}"
 
     if vacancy.get("published_at"):
-        markdown += f"\n**Опубликовано:** {vacancy['published_at']}"
+        markdown += f"\n**РћРїСѓР±Р»РёРєРѕРІР°РЅРѕ:** {vacancy['published_at']}"
 
     snippet = vacancy.get("snippet")
     if snippet and snippet.get("requirement"):
-        markdown += f"\n**Описание:** {snippet['requirement']}"
+        markdown += f"\n**РћРїРёСЃР°РЅРёРµ:** {snippet['requirement']}"
 
     employer = vacancy.get("employer")
     if employer and employer.get("name"):
-        markdown += f"\n**Работодатель:** {employer['name']}"
+        markdown += f"\n**Р Р°Р±РѕС‚РѕРґР°С‚РµР»СЊ:** {employer['name']}"
 
     salary = vacancy.get("salary")
     if salary and salary.get("from") and salary.get("to") and salary.get("currency"):
-        markdown += f"\n**Зарплата:** {salary['from']} - {salary['to']} {salary['currency']}"
+        markdown += f"\n**Р—Р°СЂРїР»Р°С‚Р°:** {salary['from']} - {salary['to']} {salary['currency']}"
 
     schedule = vacancy.get("schedule")
     if schedule and schedule.get("name"):
-        markdown += f"\n**График работы:** {schedule['name']}"
+        markdown += f"\n**Р“СЂР°С„РёРє СЂР°Р±РѕС‚С‹:** {schedule['name']}"
 
     experience = vacancy.get("experience")
     if experience and experience.get("name"):
-        markdown += f"\n**Требуемый опыт работы:** {experience['name']}"
+        markdown += f"\n**РўСЂРµР±СѓРµРјС‹Р№ РѕРїС‹С‚ СЂР°Р±РѕС‚С‹:** {experience['name']}"
 
     return markdown
 
 
 def create_keyboard_markup(vacancy: dict) -> InlineKeyboardMarkup:
     buttons_rows = list()
-    buttons_rows += [InlineKeyboardButton(text="Ссылка", url=vacancy["url"])]
+    buttons_rows += [InlineKeyboardButton(text="РЎСЃС‹Р»РєР°", url=vacancy["url"])]
     if vacancy.get("alternate_url"):
-        buttons_rows += [InlineKeyboardButton(text="Альтернативная ссылка", url=vacancy["alternate_url"])]
+        buttons_rows += [InlineKeyboardButton(text="РђР»СЊС‚РµСЂРЅР°С‚РёРІРЅР°СЏ СЃСЃС‹Р»РєР°", url=vacancy["alternate_url"])]
     return InlineKeyboardMarkup(buttons_rows)
